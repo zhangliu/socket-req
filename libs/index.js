33fcf0d87312bde406c67c93253c8525ceaac434
yunnian.zhang <yunnian.zhang@yintech.cn>
2017-11-01 10:11:58 +0800

    modify files

--- libs/index.js
+++ libs/index.js
@@ -1,46 +1,15 @@
 
-const co = require('co')
-
 const TIME_OUT = 2000
 
 class SocketReq {
   constructor(socket, receiver) {
     this.socket = socket
-    this.receiver = receiver
+    this.receiver = receiver || socket
   }
 
-  sendReq(data, method, option) {
+  sendReq(sendData, eventName, option) {
     return new Promise((resolve, reject) => {
-      this.socket.write(data)
-      const callback = resData => {
-        if ((typeof method === 'function')) {
-          co(function* run() {
-            try {
-              let checkResult = method(resData)
-              if (typeof checkResult !== 'boolean') {
-                checkResult = yield checkResult
-              }
-              if (!checkResult) return
-              this.socket.removeListener('data', callback)
-              resolve(resData)
-            } catch (err) {
-              reject(err)
-            }
-          }.bind(this))
-        }
-      }
-      this.socket.on('data', callback)
-
-      setTimeout(() => {
-        this.socket.removeListener('data', callback)
-        reject('socket获取数据超时')
-      }, TIME_OUT)
-    })
-  }
-
-  sendReqWithReceiver(data, eventName, option) {
-    return new Promise((resolve, reject) => {
-      this.socket.write(data)
+      this.socket.write(sendData)
       const callback = data => {
         resolve(data)
         this.receiver.removeListener(eventName, callback)
